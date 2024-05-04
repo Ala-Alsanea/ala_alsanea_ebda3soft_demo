5040bdbc7bd1d13036d609b89238d8955b59d5c9
Ala-Alsanea <ala0alsanea@gmail.com>
2024-05-04 16:32:45 +0300

    ref: add enums

---
+++ Persistent/Enums/AccountType.cs
@@ -0,0 +1,8 @@
+namespace ala_alsanea_ebda3soft_demo.Persistent.Enums
+{
+    public enum AccountType
+    {
+        type1,
+        type2
+    }
+}
---
+++ Persistent/Enums/UnitType.cs
@@ -0,0 +1,8 @@
+namespace ala_alsanea_ebda3soft_demo.Persistent.Enums
+{
+    public enum UnitType
+    {
+        unit1,
+        unit2,
+    }
+}
