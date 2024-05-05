4c55dfb6cb19f4fddd94626872f7e80cb83a0062
Ala-Alsanea <ala0alsanea@gmail.com>
2024-05-05 14:40:07 +0300

    feat: ceud impl for account

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -36,6 +36,58 @@ namespace ala_alsanea_ebda3soft_demo.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Create(Account account)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(account);
+            }
+            _context.Accounts.Add(account);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
+
+        public IActionResult Edit(long id)
+        {
+            Account? account = _context.Accounts.Find(id);
+            if (account == null)
+            {
+                return View("Error");
+            }
+            return View(account);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Account account)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(account);
+            }
+            _context.Accounts.Update(account);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            _context.Accounts.Remove(account);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
