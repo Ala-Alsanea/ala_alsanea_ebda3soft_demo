905f575eb43f1f0749f4791e1a5fa4d5b51cba3a
Ala-Alsanea <ala0alsanea@gmail.com>
2024-05-08 17:43:16 +0300

    fix: swap invo and recep enum

--- Persistent/Enums/InvoiceType.cs
+++ Persistent/Enums/InvoiceType.cs
@@ -2,7 +2,7 @@ namespace ala_alsanea_ebda3soft_demo.Persistent.Enums
 {
     public enum InvoiceType
     {
-        bond,
-        exchange
+        Purchase,
+        Sale
     }
 }
--- Persistent/Enums/ReceiptType.cs
+++ Persistent/Enums/ReceiptType.cs
@@ -2,7 +2,9 @@ namespace ala_alsanea_ebda3soft_demo.Persistent.Enums
 {
     public enum ReceiptType
     {
-        Purchase,
-        Sale
+
+
+        Bond,
+        Exchange
     }
 }
